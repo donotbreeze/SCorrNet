9b1be1308d31a14fc9264e88d9ce56c7af7b82bc
Mingchi Feng@CQUPT <donotbreeze@163.com>
2024-08-26 17:52:32 +0800

    Update example-smaple14.py

--- example-smaple14.py
+++ example-smaple14.py
@@ -1,4 +1,4 @@
-from Models.ECorrNet import ECorrNet
+from Models.SCorrNet import SCorrNet
 import torch
 import torchvision
 from PIL import Image
@@ -9,15 +9,15 @@ if __name__ == "__main__":
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     
-    net = ECorrNet().to(device)
-    net.load_state_dict(torch.load('weights/weights.pth', map_location=device))
+    net = SCorrNet().to(device)
+    net.load_state_dict(torch.load('weights/weights_SCorrNet.pth', map_location=device))
     net.eval()
 
 
-    ref_image = Image.open("Sample14/Sample14 Reference.tif").convert('L')
-    tar_image = Image.open("Sample14/Sample14 L3 Amp0.1.tif").convert('L')
+    ref_image = Image.open("Sample12/oht_cfrp_00.tiff").convert('L')
+    tar_image = Image.open("Sample12/oht_cfrp_08.tiff").convert('L')
     ref_image = torchvision.transforms.ToTensor()(ref_image)
-    tar_image = torchvision.transforms.ToTensor()(tar_image)[:,:ref_image.shape[-2],:ref_image.shape[-1]]
+    tar_image = torchvision.transforms.ToTensor()(tar_image)
     image = torch.cat((ref_image, tar_image), dim=0).unsqueeze(dim=0)
 
     _,_,h,w = image.shape
@@ -27,12 +27,17 @@ if __name__ == "__main__":
     mask = torch.argmax(seg[:,:,:h,:w].cpu(),dim=1)
 
     plt.figure()
-    plt.imshow(dis[0][0],cmap='jet',vmin=-0.15,vmax=0.15)
+    plt.imshow(dis[0][0],cmap='jet',vmin=dis[0][0].min(),vmax=dis[0][0].max())
     plt.colorbar()
-    plt.savefig("smaple14-dis_x.png")
+    plt.savefig("smaple12-dis_x.png")
+
+    plt.figure()
+    plt.imshow(dis[0][1],cmap='jet',vmin=dis[0][1].min(),vmax=dis[0][1].max())
+    plt.colorbar()
+    plt.savefig("smaple12-dis_y.png")
 
     plt.figure()
     plt.imshow(mask[0],cmap='gray',vmin=0,vmax=1)
-    plt.savefig("smaple14-mask.png")
+    plt.savefig("smaple12-mask.png")
 
 
